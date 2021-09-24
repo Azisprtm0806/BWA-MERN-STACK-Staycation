090a8cff1054a428ef55c7147896195d297d80e1
M Ajis Pratama <85015184+Azisprtm0806@users.noreply.github.com>
2021-09-24 20:04:06 +0700

    fix mistyped  class + complete component parts footer

---
+++ frontend-bwa/src/parts/Testimony.js
@@ -0,0 +1,59 @@
+import React from "react";
+import TestimonyAccent from "assets/images/testimonial-landingpages-frame.jpg";
+import Button from "elements/Button";
+import Star from "elements/Star";
+import landingPage from "json/landingPage";
+
+export default function Testimony() {
+  return (
+    <section className="container">
+      <div className="row align-items-center">
+        <div className="col-auto" style={{ marginRight: 60 }}>
+          <div className="testimonial-hero" style={{ margin: `30px 0 0 30px` }}>
+            <img
+              src={landingPage.testimonial.imageUrl}
+              alt="Testimonial"
+              className="position-absolute"
+              style={{ zIndex: 1 }}
+            />
+            <img
+              src={TestimonyAccent}
+              alt="Testimonial frame"
+              className="position-absolute"
+              style={{ margin: `-30px 0 0 -30px` }}
+            />
+          </div>
+        </div>
+        <div className="col">
+          <h4 style={{ marginBottom: 40 }}>{landingPage.testimonial.name}</h4>
+          <Star
+            value={landingPage.testimonial.rate}
+            width={35}
+            height={35}
+            spacing={4}
+          />
+          <h5 className="h2 font-weight-light line-height-2 my-3">
+            {landingPage.testimonial.content}
+          </h5>
+          <span className="text-gray-500">
+            {landingPage.testimonial.familyName},{" "}
+            {landingPage.testimonial.familyOccupation}
+          </span>
+
+          <div>
+            <Button
+              className="btn px-5"
+              style={{ marginTop: 40 }}
+              hasShadow
+              isPrimary
+              type="link"
+              href={`/testimonial/${landingPage.testimonial._id}`}
+            >
+              Read Their Story
+            </Button>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+}
